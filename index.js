44d3206117681f710c70d1bb54608e9a640a73fc
Oliver <olichase91@gmail.com>
2023-02-07 17:15:31 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your code here!
+
+const main = document.getElementById("main")
+main.remove()
+
+const main2 = document.getElementById("main")
+main2.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+newHeader.textContent = ("Oli is the champion")
+
+document.body.append(newHeader)
+//Damn Straight!
+
+
+
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+
+// for(let i = 0; i < 3; i++){
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul);
+
+
+// const main = document.getElementById("main");
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+
+//   const element1 = document.getElementById("main");
+// element1.style.height = "300px";
+// element1.style.backgroundColor = "#27647B";
+
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+// element.className = "pet-listing dog";
